d20d28eafbabd52854e3fc194413b1db2e1c74a0
karlijuhkam <karli.juhkam@khk.ee>
2017-12-20 12:09:40 +0200

    'array testkuvamine'

---
+++ massiivid.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Karli
+ * Date: 20.12.2017
+ * Time: 12:02
+ */
+
+$arvud = array(1,5,2,4);
+var_dump($arvud);
+echo '<hr />';
+echo '<pre>';
+print_r($arvud);
+echo '</pre>';
+?>
