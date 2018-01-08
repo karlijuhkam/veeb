9f41264239ec794eeb194cfb899beab77f0c93ea
karlijuhkam <karli.juhkam@khk.ee>
2018-01-08 09:03:11 +0200

    'arrayd array sees ja nende testväljastus'

--- massiivid2.php
+++ massiivid2.php
@@ -15,4 +15,30 @@ echo '<hr />';
 foreach ($opilane as $voti => $vaartus){
     echo $voti.' - '.$vaartus.'<br />';
 }
+
+echo '<hr />';
+
+$opilased = array(
+    array(
+        'eesnimi' => 'Mart',
+        'perenimi' => 'Lepp',
+        'vanus' => 16,
+        'klass' => 10
+    ),
+    array(
+        'eesnimi' => 'Anna',
+        'perenimi' => 'Minna',
+        'vanus' => 18,
+        'klass' => 11
+    ),
+    array(
+        'eesnimi' => 'Kari',
+        'perenimi' => 'Munasid',
+        'vanus' => 17,
+        'klass' => 10)
+);
+
+echo '<pre>';
+print_r($opilased);
+echo '</pre>';
 ?>
