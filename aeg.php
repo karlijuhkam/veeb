191bc78532f3cd292010b3b4d35b12608f7034ec
karlijuhkam <karli.juhkam@khk.ee>
2018-01-10 12:53:46 +0200

    'Loodud andmete sisestamisvorm ja aasta kontrollimise funktsioon'

---
+++ aeg.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 8.01.2018
+ * Time: 14:51
+ */
+date_default_timezone_set('Europe/Helsinki');
+echo time().'<br />';
+echo mktime(14, 56, 00, 1, 09, 2018).'<br />';
+$tana = date("H:i");
+echo $tana.'<br />';
+/*
+ * Koosta skript, mis väljastab vormi, mille kaudu
+ * kasutajal on võimalik sisestada oma sünnikuupäev
+ * Arvuta nuppu vajutamisega kasutajale antakse
+ * teada, mitu kuud, päeva, tundi ja minutid on
+ * jäänud antud tähtsa sündmuseni.
+ *
+ * Lahendus peab olema funktsionaalne - eraldi osad
+ * realiseerida funtksioonidega,
+ * võimalusel andmete salvestamiseks kasuta sobilik
+ * andmetüüp - näiteks massiivid, sõned jne
+ * */
+function sisestaAndmed(){
+    echo '<form action="'.$_SERVER['PHP_SELF'].'" method="post">';
+    echo 'Päev: <input type="text" name="paev"><br />';
+    echo 'Kuu: <input type="text" name="kuu"><br />';
+    echo '<input type="submit" value="Arvuta!">';
+    echo '</form>';
+}
+function aastaKontroll($paev, $kuu){
+    $hetkeKuu = date('n');
+    $hetkePaev = date('d');
+    if($paev < $hetkePaev and $kuu == $hetkeKuu){
+        $aasta = date('Y')+1;
+    } else{
+        $aasta = date('Y');
+    }
+    return $aasta;
+}
+sisestaAndmed();
+echo aastaKontroll($_POST['paev'], $_POST['kuu']);
